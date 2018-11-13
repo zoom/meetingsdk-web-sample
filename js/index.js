a01c97a7d209132d4d11e2b26c7b2ebebedda321
michaelZoomDA <39344486+michaelZoomDA@users.noreply.github.com>
2018-11-13 15:23:18 -0800

    update for 1.3.5

--- js/index.js
+++ js/index.js
@@ -24,6 +24,7 @@
           meetingNumber: parseInt(document.getElementById('meeting_number').value),
           userName: document.getElementById('display_name').value,
           passwd: "",
+          email: "",
           leaveUrl: "https://zoom.us",
           role: 1
         };
@@ -47,6 +48,7 @@
                     {
                         meetingNumber: meetConfig.meetingNumber,
                         userName: meetConfig.userName,
+                        userEmail: meetConfig.email,
                         signature: signature,
                         apiKey: meetConfig.apiKey,
                         success: function(res){
