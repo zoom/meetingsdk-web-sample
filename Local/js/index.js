7898858a05534755943b4c92e3560b7299e6c8b3
Jack Yang <jack.yang@zoom.us>
2019-05-09 10:25:06 +0800

    update 1.4.0

--- Local/js/index.js
+++ Local/js/index.js
@@ -4,7 +4,7 @@ console.log('checkSystemRequirements');
 console.log(JSON.stringify(ZoomMtg.checkSystemRequirements()));
 
 // it's option if you want to chenge the jssdk dependency link resources.
-// ZoomMtg.setZoomJSLib('https://dmogdx0jrul3u.cloudfront.net/1.3.7/lib', '/av'); // CDN version default 
+// ZoomMtg.setZoomJSLib('https://dmogdx0jrul3u.cloudfront.net/1.4.0/lib', '/av'); // CDN version default 
 // ZoomMtg.setZoomJSLib('http://localhost:9999/node_modules/zoomus-jssdk/dist/lib', '/av'); // Local version default
 ZoomMtg.preLoadWasm();
 ZoomMtg.prepareJssdk();
