4a03950be977e22fadac53c2c366bf966022007b
Jack Yang <jack.yang@zoom.us>
2019-12-10 09:44:41 +0800

    update 1.4.3 cdn to support ie10

---
+++ CDN/js/ie10.js
@@ -0,0 +1,81 @@
+(function(){
+
+	console.log('checkSystemRequirements');
+	console.log(JSON.stringify(ZoomMtg.checkSystemRequirements()));
+
+    // it's option if you want to change the WebSDK dependency link resources.
+    // ZoomMtg.setZoomJSLib('https://source.zoom.us/1.4.3/lib', '/av'); // CDN version default
+    // ZoomMtg.setZoomJSLib('https://jssdk.zoomus.cn/1.4.3/lib', '/av'); // china cdn option 
+    ZoomMtg.preLoadWasm();
+
+    ZoomMtg.prepareJssdk();
+
+    var API_KEY = 'YOUR_API_KEY';
+
+    /**
+        * NEVER PUT YOUR ACTUAL API SECRET IN CLIENT SIDE CODE, THIS IS JUST FOR QUICK PROTOTYPING
+        * The below generateSignature should be done server side as not to expose your api secret in public
+        * You can find an eaxmple in here: https://marketplace.zoom.us/docs/sdk/native-sdks/Web-Client-SDK/tutorial/generate-signature
+        */
+    var API_SECRET = 'YOUR_API_SECRET';
+
+
+    document.getElementById('join_meeting').addEventListener('click', function(e){
+        e.preventDefault();
+
+        if(!this.form.checkValidity()){
+            alert("Enter Name and Meeting Number");
+            return false;
+        }
+
+        var meetConfig = {
+            apiKey: API_KEY,
+            apiSecret: API_SECRET,
+            meetingNumber: parseInt(document.getElementById('meeting_number').value),
+            userName: document.getElementById('display_name').value,
+            passWord: "",
+            leaveUrl: "https://zoom.us",
+            role: 0
+        };
+
+
+        var signature = ZoomMtg.generateSignature({
+            meetingNumber: meetConfig.meetingNumber,
+            apiKey: meetConfig.apiKey,
+            apiSecret: meetConfig.apiSecret,
+            role: meetConfig.role,
+            success: function(res){
+                console.log(res.result);
+            }
+        });
+
+        ZoomMtg.init({
+            leaveUrl: 'http://www.zoom.us',
+            isSupportAV: true,
+            success: function () {
+                ZoomMtg.join(
+                    {
+                        meetingNumber: meetConfig.meetingNumber,
+                        userName: meetConfig.userName,
+                        signature: signature,
+                        apiKey: meetConfig.apiKey,
+                        userEmail: 'email@gmail.com',
+                        passWord: meetConfig.passWord,
+                        success: function(res){
+                            $('#nav-tool').hide();
+                            console.log('join meeting success');
+                        },
+                        error: function(res) {
+                            console.log(res);
+                        }
+                    }
+                );
+            },
+            error: function(res) {
+                console.log(res);
+            }
+        });
+
+    });
+
+})();
