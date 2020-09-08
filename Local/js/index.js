28b05385486f73b565eec3215cf06eb827cd4b59
Alex Che <alexandr_ch@mail.ru>
2020-09-08 13:02:34 +0300

    Update index.js

--- Local/js/index.js
+++ Local/js/index.js
@@ -15,7 +15,7 @@ const API_KEY = "YOUR_API_KEY";
 /**
  * NEVER PUT YOUR ACTUAL API SECRET IN CLIENT SIDE CODE, THIS IS JUST FOR QUICK PROTOTYPING
  * The below generateSignature should be done server side as not to expose your api secret in public
- * You can find an eaxmple in here: https://marketplace.zoom.us/docs/sdk/native-sdks/Web-Client-SDK/tutorial/generate-signature
+ * You can find an example in here: https://marketplace.zoom.us/docs/sdk/native-sdks/Web-Client-SDK/tutorial/generate-signature
  */
 const API_SECRET = "YOUR_API_SECRET";
 
