eb7baf643446ea30bbe464f272deaef289cff4c9
Jack.Yang <jack.yang@zoom.us>
2023-08-03 15:51:07 -0700

    update CDN demo dependency

--- CDN/webpack.config.dev.js
+++ CDN/webpack.config.dev.js
@@ -11,7 +11,6 @@ module.exports = {
   },
   entry: {},
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
       "process.env.NODE_ENV": JSON.stringify("development"),
       "process.env.BABEL_ENV": JSON.stringify("development"),
