8d2537b4024f786589346bced84d08f839c2975e
Jack Yang <jack.yang@zoom.us>
2026-01-06 13:35:11 -0800

    add postinstall

---
+++ Local/postinstall.js
@@ -0,0 +1,9 @@
+console.log('\n' + '='.repeat(80));
+console.log('  📦  @zoom/meetingsdk - Important Notice');
+console.log('='.repeat(80));
+console.log('\n  ⚠️  The Meeting SDK is reserved for human use cases and does not');
+console.log('      support bots or AI notetakers.\n');
+console.log('  💡  To build an AI notetaker application or access realtime media,');
+console.log('      use Zoom RTMS (Real-time media streams):\n');
+console.log('      $ npm install @zoom/rtms\n');
+console.log('='.repeat(80) + '\n');
